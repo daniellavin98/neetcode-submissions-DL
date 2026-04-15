b55e12dd7b5357bae20b0871086838eb878f5180
daniellavin98 <152566226+daniellavin98@users.noreply.github.com>
2026-04-15 11:58:53 +0100

    Add: find-target-in-rotated-sorted-array - submission-4

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-4.cs
@@ -0,0 +1,58 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+
+    //have to find the pivot - at index of smallest value 
+    //two binary searches
+    //first find the pivot 
+    //then do binary search on where we should be looking 
+
+        int l = 0; 
+        int r = nums.Length - 1; 
+
+        while(l < r){
+            int m = l + (r-l)/2; 
+
+            //first binary search
+            //left side is sorted, so min is on right 
+            if(nums[m] > nums[r]){
+                l = m + 1; 
+            }
+            else{
+                r = m;
+            }
+        }
+
+        //set the pivot to l
+        //then call binary search for either the left or right side 
+        //one will return the answer and not -1
+        int pivot = l;
+
+        int result = binarySearch(nums, target, 0, pivot-1); 
+
+        if(result != -1){
+            return result; 
+        }
+
+        return binarySearch(nums, target, pivot, nums.Length-1);
+
+    }
+
+    public int binarySearch(int[] nums, int target, int left, int right){
+
+        while(left <= right){
+            int mid = left + (right - left)/2; 
+
+            if(nums[mid] == target){
+                return mid; 
+            }
+            else if(nums[mid] > target){
+                right = mid - 1; 
+            }
+            else{
+                left = mid + 1; 
+            }
+        }
+
+        return -1; 
+    }
+}
