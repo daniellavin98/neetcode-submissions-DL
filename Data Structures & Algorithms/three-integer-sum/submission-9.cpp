52958a71ce7d1bf5c5a5c2cab7a9f878a6e8bd17
daniellavin98 <152566226+daniellavin98@users.noreply.github.com>
2026-05-21 16:53:35 +0100

    Add: three-integer-sum - submission-9

---
+++ Data Structures & Algorithms/three-integer-sum/submission-9.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        //First sort the array 
+        //Have element i be the target where -i = j+k
+        //two pointer in for loop for i 
+
+        sort(nums.begin(), nums.end()); 
+
+        vector<vector<int>> result; 
+
+        int n = nums.size(); 
+
+        // Iterate through the array as the first element of triplet
+        // Stop at length-2 since we need at least 3 elements
+        // Also stop early if current element is positive (sum can't be zero with all positive numbers)
+
+        for(int i = 0; i < n-2 && nums[i] <= 0; i++){
+            if(i > 0 && nums[i] == nums[i-1]){
+                continue; 
+            }
+
+            int l = i + 1; 
+            int r = n - 1; 
+
+            int target = -nums[i]; 
+
+            while(l < r){
+                if(nums[l] + nums[r] < target){
+                    l++; 
+                }
+                else if(nums[l] + nums[r] > target){
+                    r--; 
+                }
+                else{
+                    result.push_back({nums[i], nums[l], nums[r]}); 
+                    l++; 
+                    r--; 
+
+                    //skip duplicate values for second element 
+                    while(l < r && nums[l] == nums[l - 1]){
+                        l++; 
+                    }
+
+                    //skip duplicate values for third element
+                    while(l < r && nums[r] == nums[r + 1]){
+                        r--; 
+                    }
+                }
+            }
+
+            
+        }
+
+        return result; 
+    }
+};
